4305d2028b39d1b4ee9e265cf432ad6725ce0042
omnia-adnan <o.m.a.1514.ja@gmail.com>
2024-04-25 15:47:24 +0300

    form vild after submit print info in alert

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [userName, setuserName] = useState("");
+    const [email, setemail] = useState('');
+    const [textarea, settextarea] = useState('');
+
+return(
+<div className='card'>
+    <h2>Reviw our page</h2>
+<form onSubmit={() => alert(`name: ${userName}
+emali: ${email}     
+message: ${textarea}`)}>
+<div className='enter-info'>
+        <label> Enter your Name </label>
+        <input type='text' 
+        value={userName}
+        onChange={(e) => setuserName(e.target.value)}
+        placeholder='Fall Name'
+        required
+        />
+        </div>
+        <div className='enter-info'>
+        <label> Enter your Email </label> 
+        <input type='text' 
+        value={email}
+        onChange={(e) => setemail(e.target.value)}
+        placeholder='Official Email'
+        pattern='example@gmail.com'
+        required
+        />
+        </div>
+        <div className='enter-info'>
+        <label>Massege<br/></label>
+        <textarea
+        value={textarea}
+        onChange={(e) => settextarea(e.target.value)}
+        placeholder='comments ...'
+        rows={5}
+        required
+        />
+        </div>
+        <div className='enter-info'>
+        <button type='submit'>submit</button>
+
+</div>
+</form>    
+</div>
+);
 }
 
 export default App;
